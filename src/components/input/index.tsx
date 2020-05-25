b680e44f5d8ecb3d7f4c71c8e41266ee7e3e4c82
dnogueir <daniell_dng@hotmail.com>
2020-05-25 16:10:41 -0300

    refining form validation interface

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,8 +1,11 @@
 import React, {InputHTMLAttributes, useEffect, useRef, useState, useCallback} from 'react';
 import {IconBaseProps} from 'react-icons';
+import {FiAlertCircle} from 'react-icons/fi';
 import {useField} from '@unform/core';
 
-import {Container} from './styles';
+import {Container, Error} from './styles';
+
+import Tooltip from '../Tooltip';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
@@ -36,7 +39,7 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
     }, []);
 
     return (
-        <Container isFilled={isFilled} isFocused={isFocused}>
+        <Container isError={!! error} isFilled={isFilled} isFocused={isFocused}>
             {Icon && <Icon size={20} />}
             <input
                 onFocus={handlerInputFocus}
@@ -44,7 +47,11 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
                 defaultValue={defaultValue}
                 ref={inputRef}
                 {...rest} />
-            {error}
+            {error &&
+                <Error title={error}>
+                    <FiAlertCircle color="#c53030" size={20}/>
+                </Error>
+            }
         </Container>
     );
 };
